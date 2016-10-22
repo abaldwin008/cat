ecff166cf61c1885136be6d5e913fbaacb082844
Amy Baldwin <amybaldwin@Rogers-MacBook-Air.local>
2016-10-22 13:04:26 -0700

    modified adventures app views, show, new, index, form,controller, schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022025618) do
+ActiveRecord::Schema.define(version: 20161022184550) do
 
   create_table "adventures", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.string   "picture"
+    t.string   "location"
+    t.date     "visit"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
